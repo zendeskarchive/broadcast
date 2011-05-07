d785b9ec839c642c5fb2ea20269ace098c1b4be8
Antek Piechnik <antek.piechnik@gmail.com>
2011-05-07 21:03:41 +0200

    Added ruby 1.9.2 compatibility

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ $:.push File.expand_path("../../lib", __FILE__)
 require 'broadcast'
 require 'rspec'
 
+$:.push File.expand_path("../..", __FILE__)
 Dir.glob('spec/support/*').each { |f| require f }
