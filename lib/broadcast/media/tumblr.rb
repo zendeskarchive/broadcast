74d0b4cb515387c5033663745adb26d3bedfc231
Henry Maddocks <henry.maddocks@gmail.com>
2011-06-18 11:48:17 +1200

    Made params more consistent

--- lib/broadcast/media/tumblr.rb
+++ lib/broadcast/media/tumblr.rb
@@ -6,12 +6,13 @@ class Broadcast::Medium::Tumblr < Broadcast::Medium
     tumblr_url = URI.parse "http://www.tumblr.com"
     upload_path = "/api/write"
     
-    user_params    = {"email" => options.email, "password" => options.password, "generator" => "Broadcast Gem"}
+    app_params     = {"generator" => "Broadcast Gem"}
+    user_params    = {"email" => options.email, "password" => options.password}
     publish_params = {"state" => "published"}
-    post_params    = {"title" => message.subject, "body" => message.body, "type" => "regular"}.merge(publish_params)
+    post_params    = {"title" => message.subject, "body" => message.body, "type" => "regular"}
 #     tumblelog_params = {'group' => "#{options.group}.tumblr.com"}
   
-    params = user_params.merge(post_params)
+    params = user_params.merge(post_params).merge(publish_params).merge(user_params).merge(app_params)
 
     query = params.collect {|k, v| "#{k}=#{v}"}.join('&')
   
